91ece82a871dbf7cfe7b9fba8e0b0230e938f9d9
Sravani <sannidhikovela47@gmail.com>
2021-01-15 20:31:05 +0530

    should debug

--- Backend/auth.js
+++ Backend/auth.js
@@ -1,42 +1,82 @@
-const { query, response } = require('express') // this sql is to verify whether user is exist or not. get method in both postman , app.
-const{Pool, Cilent, Client} = require('pg')
-const connectionString = 'postgressql://postgres:srija@localhost:5432/postgres'
+const { query, response } = require('express')
+const { Pool, Client } = require('pg')
+const connectionString = 'postgressql://postgres:sravani@localhost:5434/music_streaming_service'
 const client = new Client({
-    connectionString:connectionString
+    connectionString: connectionString
 })
 client.connect()
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+//client.query('CREATE TABLE First(name varchar(20), age int)')
+//function queries(req,res){
 
-function validation(req,res){
+//console.log(list);     
+async function validation(req, res) {
     var list = [];
-    client.query('SELECT * from USERS').then((response)=>{
+    let abc = {}
+
+    await client.query('SELECT * from USERS').then((response) => {
         console.log(response.rows);
-        for (i in response.rows){
+       // abc.status = 200
+        //abc.json = { message: "succesful", response: response }
+        console.log(abc);
+        for (i in response.rows) {
             list.push(response.rows[i]['email_id']);
-            
+
         }
         var flag = 0
-         for(i=0; i<list.length; i++){
-             
-            if(req.body.email == list[i]){
+        for (i = 0; i < list.length; i++) {
+
+            if (req.body.email == list[i]) {
                 flag = 1;
-                console.log("details matched");
+                console.log("user found");
+                password = req.body.password;
+                bcrypt.genSalt(saltRounds, function (err, salt,abc) {
+                    bcrypt.hash(password, salt, function (err, hash,abc) {
+
+                        bcrypt.compare(password, response.rows[i]['password'], function (err, result) {
+                            if (result == true) {
+                                console.log('password matched');
+                                abc.status = 200
+                               abc.json = { message: "succesful", response: response }
+                            } else {
+                                console.log("password incorrect");
+                                abc.status = 400
+                                abc.json = { message: "failed", error: error }
+                            }
+                        });
+
+
+
+                    });
+                });
+
                 return;
             }
-            
-         }
-         if(flag == 0){
-             console.log("Invalid login details");
-         }
-        return res.status(200).json({message:"succesful", response:response});
-    }).catch((error)=>{client.end(); return res.status(400).json({message:"failed", error:error})})
+
+        }
+        if (flag == 0) {
+            console.log("Invalid login details");
+        }
+      //  abc.status = 200
+       // abc.json = { message: "succesful", response: response }
+
+    }).
+
+        catch((error) => {
+            client.end();
+            abc.status =400
+            abc.json = { message: "failed", error: error }
+        })
+    return res.status(abc.status).json(abc.json)
 }
 
 
-function registration(req,res){
+function registration(req, res) {
 
-      
+
     var namee;
-    var email,password,mobile;
+    var email, password, mobile;
     namee = req.body.username;
     email = req.body.email;
     password = req.body.password;
@@ -45,20 +85,20 @@ function registration(req,res){
     email = String(email);
     password = String(password);
     mobile = String(mobile);
-    
-    //response.rows.push({'name':namee, 'age':agee});
-    client.query("INSERT INTO USERS(name,email_id,password,mobile_no) VALUES ($1, $2, $3, $4)",[namee,email,password,mobile],
-    function(err, result){
-        if(err){
-            console.log(err);
-        }else{
-            console.log('row inserted');
-        }
+
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+        bcrypt.hash(password, salt, function (err, hash) {
+            client.query("INSERT INTO USERS(name,email_id,password,mobile_no) VALUES ($1, $2, $3, $4)", [namee, email, hash, mobile],
+                function (err, result) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log('row inserted');
+                    }
+                });
+        });
     });
-
-        
 }
-
-module.exports={
+module.exports = {
     validation, registration
 }
